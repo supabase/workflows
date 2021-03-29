3289c6ad56b20310e3d877f0d047d11ec418755a
Francesco Ceccon <francesco@ceccon.me>
2021-03-29 15:55:50 +0100

    chore: cache dialyzer plt files

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Workflows.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer(),
     ]
   end
 
@@ -23,6 +24,13 @@ defmodule Workflows.MixProject do
     [
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
     ]
   end
 end
