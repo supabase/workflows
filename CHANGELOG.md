d36a95bae17c7f10820e56ade2f60d5bd299ad8e
Francesco Ceccon <francesco@ceccon.me>
2021-03-29 16:07:52 +0100

    doc: add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## Unreleased
+
+### Added
+
+- Parse activities from json-like definition
+- Generate events for Task and Wait activities 
--- mix.exs
+++ mix.exs
@@ -1,18 +1,23 @@
 defmodule Workflows.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/supabase/workflows"
+  @version "0.1.0"
+
   def project do
     [
       app: :workflows,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: dialyzer(),
+      description: description(),
+      package: package(),
+      docs: docs(),
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
@@ -33,4 +38,30 @@ defmodule Workflows.MixProject do
       plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
     ]
   end
+
+  defp description() do
+    """
+    Amazon States Language workflow interpreter.
+    """
+  end
+
+  defp package() do
+    [
+      maintainers: ["The Supabase Team"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      name: "Workflows",
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/workflows",
+      source_url: @source_url,
+      extras: ["README.md", "CHANGELOG.md", "LICENSE"]
+    ]
+  end
+
 end
