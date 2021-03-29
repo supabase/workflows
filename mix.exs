0649b8f55c3b69333cfe9ffeb2921de914a321dc
Francesco Ceccon <francesco@ceccon.me>
2021-03-29 21:18:29 +0100

    chore: try fix dialyzer task

--- mix.exs
+++ mix.exs
@@ -34,10 +34,7 @@ defmodule Workflows.MixProject do
   end
 
   defp dialyzer do
-    [
-      plt_core_path: "priv/plts",
-      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
-    ]
+    []
   end
 
   defp description() do
